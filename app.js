96875330e8ae9eb120c746976a3debfc9600c86c
kgaolodevs <kgaolodev@gmail.com>
2020-07-02 19:53:35 +0200

    Added contrast functionality to color section hex titles

--- app.js
+++ app.js
@@ -7,22 +7,31 @@ let initialColors;
 
 // Functions
 function generateHex() {
-  const chars = "0123456789ABCDEF";
-  let hash = "#";
-  for (let i = 0; i < 6; i++) {
-    hash += chars[Math.floor(Math.random() * 16)];
-  }
-  return hash;
+  const hexColor = chroma.random();
+  return hexColor;
 }
 
 function randomColors() {
   colorSections.forEach((section, index) => {
     const hexText = section.children[0];
     const randomColor = generateHex();
+    section.style.background = randomColor;
+    hexText.textContent = randomColor;
+
+    // Check contrast
+    checkTextContrast(randomColor, hexText);
   });
 }
 randomColors();
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  luminance > 0.5 ? (text.style.color = "black") : (text.style.color = "white");
+}
+
 // Event listeners
-document.querySelector(".app__colors--color").addEventListener("click", () => {
-  changeBackground();
+document.querySelectorAll(".app__colors").forEach((color) => {
+  color.addEventListener("click", () => {
+    randomColors();
+  });
 });
