23645aafcd46cce51a8799152364ce02c24e2a17
kgaolodevs <kgaolodev@gmail.com>
2020-07-03 08:53:58 +0200

    Added auto updating  hsl colors to slider backgrounds

--- app.js
+++ app.js
@@ -20,8 +20,36 @@ function randomColors() {
 
     // Check contrast
     checkTextContrast(randomColor, hexText);
+
+    // Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = section.querySelectorAll(".app__colors--sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // Saturation range (scale)
+  const noSaturation = color.set("hsl.s", 0);
+  const fullSaturation = color.set("hsl.s", 1);
+  const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+  // Brightness range (scale)
+  const midBrightness = color.set("hsl.l", 0.5);
+  const scaleBrightness = chroma.scale(["black", midBrightness, "white"]);
+
+  // Update input background color
+  // prettier-ignore
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+  // prettier-ignore
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)} ,${scaleBrightness(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(204,75,75), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`;
+}
+
 randomColors();
 
 function checkTextContrast(color, text) {
